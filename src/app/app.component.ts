0b210992d78db0ef1202aafca02bc370c9df0d6f
nerioalfonso <nerioalfonso@nerio-PC.Home>
2021-08-27 13:52:01 -0400

    llamando al ep para visitantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { WhoamiServiceService } from './whoami-service.service';
+import { timeout }              from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Nerio's page";
+  visitors : number =0;
+  constructor(private whoamiService : WhoamiServiceService){}
+  ngOnInit(): void
+  {
+    this.whoamiService.getVisitors()
+    .pipe(timeout(10000))
+    .subscribe(
+      (response: any) =>
+      {
+        if(response.success)
+        {
+          this.visitors =response.payload['visitors'];
+        }
+      }
+    );
+  }
 }
--- src/app/whoami-service.service.ts
+++ src/app/whoami-service.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class WhoamiServiceService {
 
-  constructor() 
+  constructor(private http: HttpClient) 
   {
     
   }
+  getVisitors() : Observable<any>
+  {
+    return this.http.get('http://localhost:5000/visit');
+  }
+
 }
